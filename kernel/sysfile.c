d08f972750d3358a9d3ae740cc53ca4c44f693ae
laihz <2190732276@qq.com>
2024-05-27 16:56:36 +0800

    trace frame

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -72,7 +72,6 @@ sys_read(void)
   struct file *f;
   int n;
   uint64 p;
-
   if(argfd(0, 0, &f) < 0 || argint(2, &n) < 0 || argaddr(1, &p) < 0)
     return -1;
   return fileread(f, p, n);
@@ -482,5 +481,11 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+uint64
+sys_trace(void)
+{
+  
   return 0;
 }
